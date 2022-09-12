b9a1a93315b27f93d0ee7afacd2b54c5696b6e64
Adnan Hashmi <56730784+adnanhashmi09@users.noreply.github.com>
2022-09-12 16:54:54 +0530

    first commit (#1)

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2022-23 Mithra. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/forms/FormDescription.tsx
@@ -0,0 +1,46 @@
+// styles
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../../styles/components/FormDescription.module.scss';
+
+interface Props {
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonLink: string;
+}
+
+function FormDescription({
+  title,
+  description,
+  buttonText,
+  buttonLink,
+}: Props) {
+  return (
+    <header className={styles.header}>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <div className={styles.header__pattern}>
+        <Image
+          src="/asset1.svg"
+          height={130}
+          width={130}
+          alt="abstract pattern"
+        />
+      </div>
+      <button>
+        <div className={styles.ellipse}>
+          <Image src="/ellipsis.svg" height={30} width={30} alt="Circle" />
+        </div>
+        <Link href={buttonLink}>
+          <a>{buttonText}</a>
+        </Link>
+        <div className={styles.arrow}>
+          <Image src="/arrow.svg" height={35} width={40} alt="Arrow" />
+        </div>
+      </button>
+    </header>
+  );
+}
+
+export default FormDescription;
