da3973af38d5872cf053c82417abb62c4ca8d264
DecFox <mehul707gulati@gmail.com>
2022-10-19 21:22:26 +0530

    apply suggestions from code review

--- components/tables/Table.tsx
+++ components/tables/Table.tsx
@@ -12,31 +12,33 @@ interface Header {
 function Table({headers, data} : {headers: Header, data: any}) { 
     return (
         <table className={styles.table}>
-            <tr className={styles.table__row__header}>
-                {Object.values(headers).map((header: string) => {
+            <tbody>
+                <tr className={styles.table__row__header}>
+                    {Object.values(headers).map((header: string) => {
+                        return (
+                            <th className={styles.table__header} key={header}>{header}</th>
+                        );
+                    })}
+                </tr>
+                {data.map((entry: any, id: number) => {
                     return (
-                        <th className={styles.table__header} key={header}>{header}</th>
+                        <tr className={styles.table__row__content} key={id}>
+                            {Object.keys(headers).map((header: string) => 
+                                Object.entries(entry).map(([key, val]: [key: string, val: any]) => {
+                                    if(key == header) {
+                                        return (
+                                            <th className={styles.table__content} key={key}>
+                                                {key === 'txn' ? <a href="#">{val}</a> : val}
+                                            </th>
+                                        )
+                                    }
+                                }
+                            )
+                        )}
+                        </tr>
                     );
                 })}
-            </tr>
-            {data.map((entry: any, id: number) => {
-                return (
-                    <tr className={styles.table__row__content} key={id}>
-                        {Object.keys(headers).map((header: string) => 
-                            Object.entries(entry).map(([key, val]: [key: string, val: any]) => {
-                                if(key == header) {
-                                    return (
-                                        <th className={styles.table__content} key={key}>
-                                            {key === 'txn' ? <a href="#">{val}</a> : val}
-                                        </th>
-                                    )
-                                }
-                            }
-                        )
-                    )}
-                    </tr>
-                );
-            })}
+            </tbody>
         </table>
     );
 }
