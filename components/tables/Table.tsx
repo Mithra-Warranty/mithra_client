8f5904b3cde8ab0b9450d43a2e504ed05849f982
DecFox <33030671+DecFox@users.noreply.github.com>
2022-10-23 00:41:03 +0530

    feature: history component for warranty transactions (#10)
    * introduce transaction history
    
    * make table responsive
    
    * convert to link
    
    * apply suggestions from code review
    
    * apply suggestions from code review

---
+++ components/tables/Table.tsx
@@ -0,0 +1,48 @@
+import styles from '../../styles/components/TableGeneric.module.scss';
+
+// constants and variables 
+interface Header {
+    txn: string;
+    event?: string;
+    to?: string;
+    from?: string;
+    date?: string
+}
+
+function Table({headers, data} : {headers: Header, data: any}) { 
+    return (
+        <table className={styles.table}>
+            <thead>
+                <tr className={styles.table__row__header}>
+                    {Object.values(headers).map((header: string) => {
+                        return (
+                            <th className={styles.table__header} key={header}>{header}</th>
+                        );
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((entry: any, id: number) => {
+                    return (
+                        <tr className={styles.table__row__content} key={id}>
+                            {Object.keys(headers).map((header: string) => 
+                                Object.entries(entry).map(([key, val]: [key: string, val: any]) => {
+                                    if(key == header) {
+                                        return (
+                                            <td className={styles.table__content} key={key}>
+                                                {key === 'txn' ? <a href="#">{val}</a> : val}
+                                            </td>
+                                        )
+                                    }
+                                }
+                            )
+                        )}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
